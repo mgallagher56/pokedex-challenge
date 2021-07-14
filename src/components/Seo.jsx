b7cfad5373367644196bca9cc22087e715648b36
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-07-14 23:09:41 +0100

    added seo component to improve seo

---
+++ src/components/Seo.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+function Seo ({ metaDescription, meta, lang, title }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title,
+            description,
+            lang
+          }
+        }
+      }
+    `
+  )
+
+  const defaultTitle = site.siteMetadata?.title
+
+  return (
+    <Helmet
+      html={{ lang: lang }}
+      title={title || site.siteMetadata.title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      meta={[
+        {
+          name: 'description',
+          content: metaDescription || site.siteMetadata.description
+        },
+        {
+          property: 'og:title',
+          content: title || site.siteMetadata.title
+        },
+        {
+          property: 'og:description',
+          content: metaDescription || site.siteMetadata.description
+        },
+        {
+          name: 'viewport',
+          content: 'minimum-scale=1, initial-scale=1, width=device-width'
+        },
+        {
+          property: 'og:type',
+          content: 'website'
+        }
+      ]}
+    />
+  )
+}
+
+export default Seo
