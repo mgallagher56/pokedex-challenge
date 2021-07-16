fba63376fc2c3bf798287969cdb72b3f29dd4a1e
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-07-16 22:33:22 +0100

    Squashed commit of the following:
    commit f58706f28d66f98197df0262389a9dc3cc0b634b
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Fri Jul 16 22:27:00 2021 +0100
    
        added a template for pokemon details and created a page for each pokemon
    
    commit 22709a789f3045949664f280f969fb5103deed56
    Merge: 2ab8ed9 75b1340
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Fri Jul 16 17:18:07 2021 +0100
    
        Merge branch 'data-fetch' into development
    
    commit 75b1340c7247c1865f1a9d9ecb7bfbb0e665f467
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Fri Jul 16 17:16:25 2021 +0100
    
        updated listPokemon component query to match new graphql structure
    
    commit c880dcd0c0f30e411e778f4a0ff95497b6ee08e3
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Fri Jul 16 17:15:01 2021 +0100
    
        added material ui kab for addition component for pagination
    
    commit 44574f5f7741d285c144075d7ef55a457b70e1c4
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Fri Jul 16 17:13:52 2021 +0100
    
        updated pokemon data fetch to created paginated list automatically
    
    commit 321beef0463eb44157edb2503225cc30b7e9a8e8
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Fri Jul 16 09:26:27 2021 +0100
    
        added chained fetch request to pull in pokemon in pages and add nodes to graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,54 +1,102 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
 const fetch = require('node-fetch')
-
-const gqlQuery = `query pokemons($limit: Int, $offset: Int) {
-  pokemons(limit: $limit, offset: $offset) {
-    count
-    next
-    previous
-    status
-    message
-    results {
-      url
-      name
-      image
-      id
-    }
-  }
-}`
-
-const gqlVariables = {
-  limit: 151,
-  offset: 0
-}
+const path = require('path')
 
 exports.sourceNodes = async ({
-  actions: { createNode },
+  actions: { createNode, createPage },
   createContentDigest
 }) => {
-  const result = await fetch('https://graphql-pokeapi.vercel.app/api/graphql', {
-    credentials: 'omit',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query: gqlQuery,
-      variables: gqlVariables
-    }),
-    method: 'POST'
+  await FetchPaginatedPokemon()
+    .then(pokemonData => {
+      createNode({
+        nodes: pokemonData,
+        id: 'pokemon-list',
+        parent: null,
+        children: [],
+        internal: {
+          type: 'pokemonData',
+          contentDigest: createContentDigest(pokemonData)
+        }
+      })
+    })
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const PageTemplate = path.resolve('./src/templates/PokemonDetail.jsx')
+
+  const result = await graphql(
+    `
+      query pokemonList {
+        pokemonData {
+          nodes {
+            results {
+              name
+              id
+              image
+            }
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild('Error while running GraphQL query.')
+    return
+  }
+
+  result.data.pokemonData.nodes.map(page => {
+    page.results.map(pokemon => {
+      createPage({
+        path: `/pokemon/${pokemon.name}`,
+        component: PageTemplate
+      })
+      return null
+    })
+    return null
   })
-  const resultData = await result.json()
-  const pokemons = resultData.data.pokemons.results
-  createNode({
-    pokemons: pokemons,
-    id: 'example-build-time-data',
-    parent: null,
-    children: [],
-    internal: {
-      type: 'pokemonData',
-      contentDigest: createContentDigest(resultData)
-    }
-  })
+}
+
+const FetchPaginatedPokemon = async (pokemonPerPage = 20, pageQuantity = 8) => {
+  const pokeArr = []
+  let offset = 0
+  let url = `https://pokeapi.co/api/v2/pokemon?limit=${pokemonPerPage}&offset=${offset}`
+  for (let i = 0; i < pageQuantity; i++) {
+    await asyncFetch(url)
+      .then(allPokemon => {
+        allPokemon.results.forEach(pokemon => {
+          asyncFetch(pokemon.url)
+            .then(pokemonDetail => {
+              asyncFetch(pokemonDetail.species.url)
+                .then(species => {
+                  pokemon.id = pokemonDetail.id
+                  pokemon.image = pokemonDetail.sprites.other['official-artwork'].front_default
+                  pokemon.types = pokemonDetail.types
+                  pokemon.weight = pokemonDetail.weight
+                  pokemon.height = pokemonDetail.height
+
+                  if (species.flavor_text_entries[0].language.name === 'en') {
+                    pokemon.description = species.flavor_text_entries[0].flavor_text
+                  } else if (species.flavor_text_entries[1].language.name === 'en') {
+                    pokemon.description = species.flavor_text_entries[1].flavour_text
+                  } else {
+                    pokemon.description = species.flavor_text_entries[2].flavorr_text
+                  }
+                }).catch(err => {
+                  console.error(err);
+                })
+            })
+        })
+        pokeArr.push(allPokemon)
+        url = allPokemon.next
+      })
+    offset += pokemonPerPage
+  }
+  return pokeArr
+}
+
+async function asyncFetch(url) {
+  let result = await fetch(url)
+  result = result.json()
+  return result
 }
---
+++ src/templates/PokemonDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Seo from '../components/Seo'
+
+const PokemonDetail = ({ data }) => {
+  return (
+    <main>
+      <Seo />
+      <h1>Test</h1>
+    </main>
+  )
+}
+
+export default PokemonDetail
