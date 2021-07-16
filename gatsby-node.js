321beef0463eb44157edb2503225cc30b7e9a8e8
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-07-16 09:26:27 +0100

    added chained fetch request to pull in pokemon in pages and add nodes to graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,54 +1,62 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
 const fetch = require('node-fetch')
-
-const gqlQuery = `query pokemons($limit: Int, $offset: Int) {
-  pokemons(limit: $limit, offset: $offset) {
-    count
-    next
-    previous
-    status
-    message
-    results {
-      url
-      name
-      image
-      id
-    }
-  }
-}`
-
-const gqlVariables = {
-  limit: 151,
-  offset: 0
-}
 
 exports.sourceNodes = async ({
   actions: { createNode },
   createContentDigest
 }) => {
-  const result = await fetch('https://graphql-pokeapi.vercel.app/api/graphql', {
-    credentials: 'omit',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      query: gqlQuery,
-      variables: gqlVariables
-    }),
-    method: 'POST'
-  })
-  const resultData = await result.json()
-  const pokemons = resultData.data.pokemons.results
-  createNode({
-    pokemons: pokemons,
-    id: 'example-build-time-data',
-    parent: null,
-    children: [],
-    internal: {
-      type: 'pokemonData',
-      contentDigest: createContentDigest(resultData)
-    }
-  })
+  FetchPaginatedPokemon()
+    .then(pokemons => {
+      createNode({
+        pokemons: pokemons,
+        id: 'pokemon-list',
+        parent: null,
+        children: [],
+        internal: {
+          type: 'pokemonData',
+          contentDigest: createContentDigest(pokemons)
+        }
+      })
+    })
+}
+
+const FetchPaginatedPokemon = async () => {
+  const pokeObj = { page: {} }
+  let offset = 0
+
+  for (let i = 0; i < 8; i++) {
+    await asyncFetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`)
+      .then(async allPokemon => {
+        await allPokemon.results.forEach(pokemon => {
+          asyncFetch(pokemon.url)
+            .then(async pokemonDetail => {
+              await asyncFetch(pokemonDetail.species.url)
+                .then(species => {
+                  pokemon.id = pokemonDetail.id
+                  pokemon.image = pokemonDetail.sprites.other['official-artwork'].front_default
+                  pokemon.types = pokemonDetail.types
+                  pokemon.weight = pokemonDetail.weight
+                  pokemon.height = pokemonDetail.height
+
+                  if (species.flavor_text_entries[0].language.name === 'en') {
+                    pokemon.description = species.flavor_text_entries[0].flavor_text
+                  } else if (species.flavor_text_entries[1].language.name === 'en') {
+                    pokemon.description = species.flavor_text_entries[1].flavour_text
+                  } else {
+                    pokemon.description = species.flavor_text_entries[2].flavorr_text
+                  }
+                })
+            })
+        })
+        pokeObj.page['page-' + i] = allPokemon
+      })
+    offset += 20
+    pokeObj.page['page-' + i].id = i
+  }
+  return pokeObj
+}
+
+async function asyncFetch(url) {
+  let result = await fetch(url)
+  result = await result.json()
+  return result
 }
