f180bf0671b3c198963b9b31ca1fb196bc4829b2
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-07-18 00:47:40 +0100

    added search functionality to pokemon list

--- src/components/ListPokemon.jsx
+++ src/components/ListPokemon.jsx
@@ -1,5 +1,8 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { useStaticQuery, graphql, Link } from 'gatsby'
+import { useDebouncedCallback } from 'use-debounce'
+import { makeStyles } from '@material-ui/core/styles'
+
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
@@ -7,42 +10,62 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
+import { TextField } from '@material-ui/core'
 import Pagination from '@material-ui/lab/Pagination'
-// import Link from '@material-ui/core/Link'
 
 // styles
 const useStyles = makeStyles((theme) => ({
+  input: {
+    paddingBottom: theme.spacing(8),
+    width: '100%'
+  },
   cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8)
+    paddingBottom: theme.spacing(8),
+    paddingTop: theme.spacing(8)
   },
   card: {
-    height: '100%',
     display: 'flex',
     flexDirection: 'column',
+    height: '100%',
     transition: 'all .3s ease-out',
     transitionProperty: 'box-shadow, transform',
     '&:hover': {
       boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
       transform: 'translate3d(0px, -2px, 0px)'
+    },
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'row',
+      maxHeight: '100px'
     }
   },
   cardTitle: {
     textTransform: 'capitalize'
   },
   cardMedia: {
-    paddingTop: '100%'
+    paddingTop: '100%',
+    [theme.breakpoints.down('xs')]: {
+      backgroundSize: 'contain',
+      height: '100px',
+      paddingTop: '0',
+      width: '33%'
+    }
   },
   cardContent: {
-    flexGrow: 1
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center'
   },
   pagination: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingBottom: theme.spacing(8),
+    paddingTop: theme.spacing(8)
+  },
+  searchError: {
+    textAlign: 'center',
+    paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(2)
   }
 }))
 
@@ -50,69 +73,131 @@ const useStyles = makeStyles((theme) => ({
 const ListPokemon = () => {
   const { pokemonData } = useStaticQuery(
     graphql`
-      query pokemonList {
-        pokemonData {
-          nodes {
-            results {
-              name
-              id
-              image
+    query pokemonList {
+      pokemonData {
+        nodes {
+          results {
+            name
+            id
+            image {
+              url
             }
           }
         }
       }
+    }
     `
   )
+  const paginatedResults = pokemonData.nodes
+  const allPokemon = []
+  paginatedResults.map(page => {
+    page.results.map(pokemon => {
+      return allPokemon.push(pokemon)
+    })
+    return allPokemon
+  })
 
-  console.log(pokemonData)
+  const filterPokemonList = (searchValue, allPokemonList) => {
+    return allPokemonList.filter((pokemon) => {
+      if (pokemon.name.includes(searchValue)) {
+        return pokemon
+      }
+      return null
+    })
+  }
+
+  const numberOfPages = (pokemonList) => {
+    const pokemonNum = pokemonList.length
+    return pokemonNum % 20 === 0 ? pokemonNum / 20 : (pokemonNum / 20) + 1
+  }
+
+  const [filteredPokemon, setFilteredPokemon] = useState(allPokemon)
+  const [pages, setPages] = useState(numberOfPages(allPokemon))
+
+  const filterPokemonCards = (filterValue) => {
+    const filteredPokemon = filterPokemonList(filterValue, allPokemon)
+    setFilteredPokemon(filteredPokemon)
+    setPages(numberOfPages(filteredPokemon))
+  }
+
+  const resetSearchHandler = () => {
+    filterPokemonCards('')
+    document.getElementById('pokemon-search').value = ''
+  }
+
+  const debounceFilter = useDebouncedCallback((value) => {
+    filterPokemonCards(value)
+  }, 150)
 
   const {
+    input,
     cardGrid,
     card,
     cardTitle,
     cardMedia,
     cardContent,
-    pagination
+    pagination,
+    searchError
   } = useStyles()
+
   // markup
   return (
     <Container className={cardGrid} maxWidth='md'>
+      <TextField
+        id='pokemon-search'
+        className={input}
+        label='Search'
+        type='search'
+        onChange={(e) => debounceFilter(e.target.value)}
+      />
+      {pages === 0
+        ? <div className={searchError}>
+          <Typography variant='h5' component='h5' className={searchError}>
+            Opps! Looks like your search didn't catch any Pokemon
+          </Typography>
+          <Button
+            variant='contained'
+            color='secondary'
+            className={searchError}
+            onClick={() => resetSearchHandler()}
+          >
+            Catch EM' ALl
+          </Button>
+        </div>
+        : null}
       <Grid container spacing={4}>
-
-        {pokemonData.nodes.map(page => {
+        {filteredPokemon.map(pokemon => {
+          const { id, name, image } = pokemon
           return (
-            page.results.map(pokemon => {
-              const { id, name, image } = pokemon
-              return (
-                <React.Fragment key={id}>
-                  <Grid item xs={12} sm={6} md={4}>
-                    <Link to={`/pokemon/${name}`}>
-                      <Card className={card}>
-                        <CardMedia
-                          className={cardMedia}
-                          image={image}
-                          title='Image title'
-                        />
-                        <CardContent className={cardContent}>
-                          <Typography gutterBottom variant='h4' component='h4' className={cardTitle}>
-                            {name}
-                          </Typography>
-                        </CardContent>
-                        <CardActions>
-                          <Button color='primary'>
-                            View details
-                          </Button>
-                        </CardActions>
-                      </Card>
-                    </Link>
-                  </Grid>
-                </React.Fragment>
-              )
-            })
+            <React.Fragment key={id}>
+              <Grid id={pokemon.name} item xs={12} sm={4} md={3}>
+                <Link to={`/pokemon/${name}`}>
+                  <Card className={card}>
+                    <CardMedia
+                      className={cardMedia}
+                      image={image.url}
+                      title='Image title'
+                    />
+                    <CardContent className={cardContent}>
+                      <Typography variant='h5' component='h5' className={cardTitle}>
+                        {name}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button color='primary'>
+                        View details
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Link>
+              </Grid>
+            </React.Fragment>
           )
         })}
       </Grid>
-      <Pagination className={pagination} count={10} color='secondary' />
+      {pages !== 0
+        ? <Pagination className={pagination} count={pages} color='secondary' />
+        : null}
     </Container>
   )
 }
