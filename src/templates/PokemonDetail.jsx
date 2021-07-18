bb013d10c398c50e13e06dec3e7c7a68b4cf9ad3
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-07-18 23:13:35 +0100

    Squashed commit of the following:
    commit 60df546a3ae6a308a31d850039aa538ffbc5e264
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Sun Jul 18 23:13:02 2021 +0100
    
        added header component to pages
    
    commit 562dd916d8b0b340a899a1d0583ccd3565dc0864
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Sun Jul 18 23:12:30 2021 +0100
    
        added simple header component
    
    commit 56c580a7355e33bf96ab4b4ceb37c07182bec7b0
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Sun Jul 18 22:38:48 2021 +0100
    
        removed console log. changed card detail image to contain to fit into container
    
    commit 2b3ebc7b6b50ec131f30a0b8dde8a98c50653c48
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Sun Jul 18 22:28:31 2021 +0100
    
        ipdated card styling
    
    commit 6e145c380b68aad55c4b5906222bc2e2e7c5df78
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Sun Jul 18 22:26:37 2021 +0100
    
        updated 404 page
    
    commit 1560fbaca52932eda71e110b65aa1b3984e95b83
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Sun Jul 18 22:26:20 2021 +0100
    
        added card for pokemon details
    
    commit 564dc9c751a9a02b3d44b87e2138fbc62c9fec6b
    Author: Marc Gallagher-Reid <marc.gr@velocitypartners.com>
    Date:   Sun Jul 18 19:58:13 2021 +0100
    
        added pokemon card component

--- src/templates/PokemonDetail.jsx
+++ src/templates/PokemonDetail.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import { getImage } from 'gatsby-plugin-image'
+
 import Seo from '../components/Seo'
+import { Container } from '@material-ui/core'
+import PokemonDetailCard from '../components/PokemonDetailCard'
+import { makeStyles } from '@material-ui/core/styles'
+import SimplifiedHeader from '../components/SimpleHeader'
 
 const PokemonDetail = ({ data }) => {
+  const { name, remoteImage, image, description, pokemonNumber } = data.allPokemon.nodes[0]
+  const gatsbyImg = getImage(remoteImage)
+
   return (
     <main>
       <Seo />
-      <h1>Test</h1>
+      <SimplifiedHeader title={name} btnText='See all Pokemon' />
+      <Container maxWidth='md'>
+        <PokemonDetailCard
+          gatsbyImg={gatsbyImg}
+          fallbackImg={image}
+          title={name}
+          description={description}
+          id={pokemonNumber}
+        />
+      </Container>
     </main>
   )
 }
+export const [pokemonDetails] = graphql`
+query ($name: String!) {
+  allPokemon(filter: { name: { eq: $name } }) {
+    nodes {
+      name
+      description
+      image
+      pokemonNumber
+      remoteImage {
+        childImageSharp {
+          gatsbyImageData(
+            formats: [WEBP, AVIF, AUTO]
+            placeholder: BLURRED
+            tracedSVGOptions: {background: "#1B8FD3", color: "#FF6F58"}
+          )
+        }
+      }
+    }
+  }
+}
+`
 
 export default PokemonDetail
