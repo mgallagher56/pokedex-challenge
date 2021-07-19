e6c28d80a808436a5d929c2e7cadc6d8f7d3ad1b
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-07-19 01:03:00 +0100

    made search string lowercase to match pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 import { Link } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { makeStyles } from '@material-ui/core/styles'
